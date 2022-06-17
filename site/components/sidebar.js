131a86f2c368e752a2adc60cc99cbeab5f0e167a
nightisyang <shenyangc86@gmail.com>
2022-06-17 15:50:29 +0800

    fixed menu button

--- site/components/sidebar.js
+++ site/components/sidebar.js
@@ -1,34 +1,13 @@
 import React, { Component } from "react";
 import Link from "next/link";
 import styles from "../styles/Sidebar.module.css";
-
 import { slide as Menu } from "react-burger-menu";
 
 const HamburgerMenu = () => (
   <div className="relative p-2">
-    <Menu
-      customBurgerIcon={<HamburgerIcon />}
-      width={"auto"}
-      className="left-0 top-20"
-    >
+    <Menu>
       <Links />
     </Menu>
-  </div>
-);
-
-const HamburgerIcon = () => (
-  <div className="p-1/2">
-    <svg
-      className="w-8 h-8 text-gray-500"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path d="M4 6h16M4 12h16M4 18h16"></path>
-    </svg>
   </div>
 );
 
@@ -37,27 +16,18 @@ export const Links = () => (
     <Link href="/">
       <a>Home</a>
     </Link>
-    <br></br>
     <Link href="/aboutme">
       <a>About me</a>
     </Link>
-    <br></br>
-
     <Link href="/blog">
       <a>Blog</a>
     </Link>
-    <br></br>
-
     <Link href="/projects">
       <a>Projects</a>
     </Link>
-    <br></br>
-
     <Link href="/contact">
       <a>Contact Me</a>
     </Link>
-    <br></br>
-
     {/* <Link href="/">
       <a className="font-bold p-4">Home</a>
     </Link>
