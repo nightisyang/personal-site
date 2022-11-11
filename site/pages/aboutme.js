cef0092ab142c59802a14f4eb4a0d4365f68c37d
nightisyang <shenyangc86@gmail.com>
2022-11-11 16:35:32 +0800

    update about me, minor blog edit

--- site/pages/aboutme.js
+++ site/pages/aboutme.js
@@ -2,6 +2,7 @@ import Header from "../components/header";
 import Layout from "../components/layout";
 import Sidebar from "../components/sidebar";
 import ContentPage from "../components/contentPage";
+import Link from "next/link";
 
 export default function AboutMe() {
   return (
@@ -10,61 +11,44 @@ export default function AboutMe() {
       <p>Curious with some daredevil tendencies.</p>
       <p>
         Quitting a 10 year career in agriculture to pursue a new interest does
-        have some similarity in pushing the limits of the body in running a 70km
+        draw some similarity in pushing the limits of the body in running a 70km
         ultramarathon as a regular guy who has never done a full 42km run.
       </p>
       <p>
-        A geneticist by training who wondered into the world of agriculture R&D
-        realizing that there's much more in the world than lab work, performing
-        data analysis from field trials and consulting stakeholders on the
-        interaction of plants, climate, terrain, pests and diseases through a
-        lifecycle of a plant. Though that chapter has ended, I do appreciate the
-        opportunity that came with the job to travel to some areas that in the
-        country that are not often visited and to hear and work together with
-        leading experts in the industry.
+        A geneticist by training, that mostly tinkered in the lab, wondered into
+        the world of agricultural R&D, performed data analysis from field
+        trials, consulting stakeholders on the interaction of plants, climate,
+        terrain, pests and diseases throughout the lifecycle of a plant. Studied
+        the origin and importance of inanimate rocks, sand, silt and clay - the
+        very thing allowed entire civilizations to be fed and built upon.
       </p>
       <p>
-        Who knew that a geneticist that deals with information encoded within
-        all living things would also be interested by origin and the importance
-        of inanimate rocks, sand, silt and clay - the very thing allows entire
-        civilizations to be fed and built upon.
+        The slow pace of R&D for a perrenial crop such as oil palm did provide
+        career stability and the initial novelty that came with it satisfied
+        curiosity. With time, novelty worn off, coupled with the slow R&D
+        progress of a perrenial crop that is usually measured in years naturally
+        slowed personal progress. Perhaps I should have taken cue and made my
+        move earlier when I was the defacto "IT" guy at work troubleshooting
+        printer and hardware issues. Better late than never. I'm grateful and
+        appreciate the opportunities it provided, but it was time to move on.
       </p>
       <p>
-        My new journey is fairly recent starting in early 2022, but I did have
-        an early start in hacking psp consoles using guides on sketchy sites in
-        my youth and installing OS that I had no use for. I also should taken
-        cue and made my move earlier when I was the defacto "IT" guy at work
-        troubleshooting printer and hardware issues.
+        My journey as a self-studied coder/programmer started early 2022, feel
+        free to look around in <Link href="/projects">projects</Link> and{" "}
+        <Link href="/blog">blog</Link> for the latest update!
       </p>
       <p>Likes dry/dad humour and enjoyor of puns, lots of puns.</p>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <img
           src="https://media.tenor.com/hsgug-TgdT4AAAAd/matrix-puns.gif"
           alt="Matrix Puns"
-          style={{ width: "100%" }}
-          // style={{ justifyContent: "center" }}
+          style={{ width: "80%" }}
         ></img>
       </div>
-      {/* <div
-        class="tenor-gif-embed"
-        data-postid="8455327"
-        data-share-method="host"
-        data-aspect-ratio="2.27273"
-        data-width="50%"
-      >
-        <a href="https://tenor.com/view/matrix-puns-reddit-gif-8455327">
-          Matrix Puns GIF
-        </a>
-        from <a href="https://tenor.com/search/matrix-gifs">Matrix GIFs</a>
-      </div>
-      <script
-        type="text/javascript"
-        async
-        src="https://tenor.com/embed.js"
-      ></script> */}
       <p>
-        Familiar with Javascript, Node.js/Express, React, React Native, SQL,
-        MongoDB. Exploring Python. This will be updated accordingly.
+        Familiar with Javascript (primary), Python, Data Stuctures and
+        Algorithms, Node.js/Express, HTML, CSS, React, React Native, SQL,
+        MongoDB.
       </p>
     </div>
   );
